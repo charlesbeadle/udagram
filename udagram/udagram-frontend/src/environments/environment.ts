070a5f0eeb7133ce961ef322b0627b600fbee178
Charles Beadle <charles.beadle@protonmail.com>
2023-06-11 21:47:55 -0400

    edit conf file

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://udagram-manual.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
