a8c98fad3c08d071dd7d2ab77cb77c517bef4d0d
Charles Beadle <charles.beadle@protonmail.com>
2023-06-11 20:06:15 -0400

    various changes

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  // apiHost: 'http://localhost:8080/api/v0',
-  apiHost: 'http://udagram-manual.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://localhost:3000/api/v0'
 };
 
 /*
